7fe906066f2d9f599c2fa0b69f925242e0e8e1dc
James Faske <jamesfaske@yahoo.com>
2021-04-29 21:13:44 -0500

    adding twitter controller

---
+++ WebApi/Controllers/TwitterController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Features;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TwitterController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public TwitterController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("start-stream")]
+        public async Task<IActionResult> StartStream()
+        {
+            var response = await _mediator.Send(new StartStream.Command());
+            return Ok(response);
+        }
+
+    }
+}
