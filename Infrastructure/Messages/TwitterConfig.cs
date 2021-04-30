9fc9c6c40ebea2ae78c588aa5202d8eb177ee675
James Faske <jamesfaske@yahoo.com>
2021-04-29 21:12:58 -0500

    adding twitter service

---
+++ Infrastructure/Messages/TwitterConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Messages
+{
+    public class TwitterConfig
+    {
+        public string ConsumerKey { get; set; }
+        public string ConsumerSecret { get; set; }
+        public string AccessToken { get; set; }
+        public string AccessTokenSecret { get; set; }
+
+    }
+}
---
+++ Infrastructure/Messages/TwitterService.cs
@@ -0,0 +1,33 @@
+﻿using Application.Interfaces;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tweetinvi;
+
+namespace Infrastructure.Messages
+{
+    public class TwitterService : ITwitterService
+    {
+        public TwitterClient _twitterClient { get; set; }
+
+        public TwitterService(IOptionsMonitor<TwitterConfig> twitterConfig)
+        {
+            var config = twitterConfig.CurrentValue;
+            _twitterClient = new TwitterClient(config.ConsumerKey, config.ConsumerSecret, config.AccessToken, config.AccessTokenSecret);
+        }
+
+        public async Task StartStreamAsync()
+        {
+            var sampleStream = _twitterClient.Streams.CreateSampleStream();
+            sampleStream.TweetReceived += (sender, eventArgs) =>
+            {
+                Console.WriteLine(eventArgs.Tweet);
+            };
+
+            await sampleStream.StartAsync();
+        }
+    }
+}
