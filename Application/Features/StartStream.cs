0d6bbd5d7b9ecffd4dc51afe9da8b140c6cc850a
James Faske <jamesfaske@yahoo.com>
2021-04-29 21:13:25 -0500

    adding start stream

---
+++ Application/Features/StartStream.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Interfaces;
+using Application.Wrappers;
+using MediatR;
+
+namespace Application.Features
+{
+    public class StartStream
+    {
+        public class Command : IRequest<Response>
+        {
+        }
+
+        public class Response : ResponseBase
+        {
+        }
+
+        public class CommandHandler : IRequestHandler<Command, Response>
+        {
+            private readonly ITwitterService _twitterService;
+
+            public CommandHandler(ITwitterService twitterService)
+            {
+                _twitterService = twitterService;
+            }
+
+            public async Task<Response> Handle(Command cmd, CancellationToken cancellationToken)
+            {
+                var response = new Response();
+
+                try
+                {
+                    await _twitterService.StartStreamAsync();
+                }
+                catch (Exception ex)
+                {
+                    response.Errors.Add($"An error occurred reading the stream: {ex.Message}");
+                    
+                }
+
+                return response;
+            }
+        }
+    }
+}
---
+++ Application/Wrappers/ResponseBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Wrappers
+{
+    public class ResponseBase
+    {
+        public bool IsError => Errors != null && Errors.Count > 0;
+        public List<string> Errors { get; set; } = new List<string>();
+    }
+}
